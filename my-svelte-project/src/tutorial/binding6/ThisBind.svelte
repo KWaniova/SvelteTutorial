304a28e6e967de3b94f82fcbab33580746db687b
Krystyna Waniova <krystyna.waniova@gmail.com>
2022-02-09 14:38:03 +0100

    initial

---
+++ my-svelte-project/src/tutorial/binding6/ThisBind.svelte
@@ -0,0 +1,19 @@
+<script>
+  import InputField from "./InputField.svelte";
+
+  let field;
+</script>
+
+<InputField bind:this={field} />
+
+<button on:click={() => field.focus()}>Focus field</button>
+
+<!-- <script>
+	let input;
+
+	export function focus() {
+		input.focus();
+	}
+</script>
+
+<input bind:this={input} /> -->
